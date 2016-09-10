9b27acec02a720292e12892976ba1b15a22ebbb6
s7067021f <s7067021f@gmail.com>
2016-09-10 05:11:18 -0400

    comment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 $("#main").append("Michael Sutherland");
+
+var awesomeThoughts = "I am Michael and I am AWESOME!";
+console.log(awesomeThoughts);
