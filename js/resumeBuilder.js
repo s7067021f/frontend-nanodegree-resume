0401661997e4585a7060fecfe89664289ae9fdf2
s7067021f <s7067021f@gmail.com>
2016-09-18 01:06:44 -0400

    comment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -16,7 +16,7 @@ var bio = {
   "name": "Michael Sutherland",
   "role": "Monkey",
   "contactInfo": "myemailaddress@gmail.com",
-  "pictureUrl": "https://www.google.dk/imgres?imgurl=http%3A%2F%2Fichef.bbci.co.uk%2Fnews%2F660%2Fcpsprodpb%2F025B%2Fproduction%2F_85730600_monkey2.jpg&imgrefurl=http%3A%2F%2Fwww.bbc.com%2Fnews%2Fworld-asia-34346092&docid=GNcRdxA225nDDM&tbnid=MTZ1sfXpGRKpfM%3A&w=660&h=660&hl=en&bih=907&biw=1727&ved=0ahUKEwiMuNuKj5jPAhXJRI8KHaTJBh0QMwg0KAMwAw&iact=mrc&uact=8",
+  "pictureUrl": "http://ichef.bbci.co.uk/news/660/cpsprodpb/025B/production/_85730600_monkey2.jpg",
   "welcomeMessage": "Hello, this is a welcome message",
   "skills": ["awesomeness","programming"]
 }
@@ -30,7 +30,7 @@ $("#header").prepend(formattedName);
 var welcomeMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 $("#header").append(welcomeMessage);
 
-var contactInfo = HTMLcontactGeneric.replace("%data",bio.contactInfo);
+var contactInfo = HTMLcontactGeneric.replace("%data%",bio.contactInfo);
 $("#header").append(contactInfo);
 
 var pictureUrl = HTMLbioPic.replace("%data%",bio.pictureUrl);
