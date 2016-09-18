d03568c278a03a379a8dff9830e3cbe0074da5af
s7067021f <s7067021f@gmail.com>
2016-09-18 01:02:32 -0400

    comment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,23 +12,31 @@ var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 $("#main").append(funThoughts);
 */
 
-var formattedRole = HTMLheaderRole.replace("%data%","Code Monkey");
-$("#header").prepend(formattedRole);
-
-var formattedName = HTMLheaderName.replace("%data%","Mike Sutherland");
-$("#header").prepend(formattedName);
-
 var bio = {
   "name": "Michael Sutherland",
   "role": "Monkey",
   "contactInfo": "myemailaddress@gmail.com",
-  "picture url": "https://www.google.dk/imgres?imgurl=http%3A%2F%2Fichef.bbci.co.uk%2Fnews%2F660%2Fcpsprodpb%2F025B%2Fproduction%2F_85730600_monkey2.jpg&imgrefurl=http%3A%2F%2Fwww.bbc.com%2Fnews%2Fworld-asia-34346092&docid=GNcRdxA225nDDM&tbnid=MTZ1sfXpGRKpfM%3A&w=660&h=660&hl=en&bih=907&biw=1727&ved=0ahUKEwiMuNuKj5jPAhXJRI8KHaTJBh0QMwg0KAMwAw&iact=mrc&uact=8",
+  "pictureUrl": "https://www.google.dk/imgres?imgurl=http%3A%2F%2Fichef.bbci.co.uk%2Fnews%2F660%2Fcpsprodpb%2F025B%2Fproduction%2F_85730600_monkey2.jpg&imgrefurl=http%3A%2F%2Fwww.bbc.com%2Fnews%2Fworld-asia-34346092&docid=GNcRdxA225nDDM&tbnid=MTZ1sfXpGRKpfM%3A&w=660&h=660&hl=en&bih=907&biw=1727&ved=0ahUKEwiMuNuKj5jPAhXJRI8KHaTJBh0QMwg0KAMwAw&iact=mrc&uact=8",
   "welcomeMessage": "Hello, this is a welcome message",
   "skills": ["awesomeness","programming"]
 }
 
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+$("#header").prepend(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+$("#header").prepend(formattedName);
+
 var welcomeMessage = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 $("#header").append(welcomeMessage);
+
+var contactInfo = HTMLcontactGeneric.replace("%data",bio.contactInfo);
+$("#header").append(contactInfo);
+
+var pictureUrl = HTMLbioPic.replace("%data%",bio.pictureUrl);
+$("#header").append(pictureUrl);
+
+
 
 
 
