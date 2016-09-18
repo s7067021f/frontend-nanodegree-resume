8154f86c7fd53b23d69c79d948989e35a0cef758
s7067021f <s7067021f@gmail.com>
2016-09-18 07:24:04 -0400

    comment

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -35,8 +35,8 @@ $("#header").append(welcomeMessage);
 var contactEmail = HTMLemail.replace("%data%",bio.contactEmail);
 $("#header").append(contactEmail);
 
-var location = HTMLlocation.replace("%data%",bio.location);
-$("#header").append(location);
+var locn = HTMLlocation.replace("%data%",bio.location);
+$("#header").append(locn);
 
 //var twitter = HTMLtwitter.replace("%data%",bio.twitter);
 //$("#header").append(twitter);
